f06bf5ecc5c1c599c98161a9ef68655f84031b33
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-23 23:32:33 -0800

    add submodule to windows-features-add repo

--- Configure-WindowsFeatures.ps1
+++ Configure-WindowsFeatures.ps1
@@ -1,16 +1,22 @@
-Write-Host "Runnng this script will install/enable the optional Windows features of Hyper-V and Containers."
+Write-Host "Running this script will install/enable the optional Windows features for Virtual Machines, WSL, and Powershell 2.0..."
 Write-Host "After the Windows features are enabled this script will force a system reboot." -ForegroundColor Magenta
 
-if($(Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V -Online).State -ieq 'disabled')
-{
-    Write-Host "Installing Microsoft Hyper V feature." -ForegroundColor DarkCyan
+if ($(Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online).State -ieq 'disabled') {
+    Write-Host "Installing Microsoft Hyper-V and VM features." -ForegroundColor DarkCyan
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
+    Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All -NoRestart
+    Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -NoRestart
+} 
+
+if ($(Get-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online).State -ieq 'disabled') {
+    Write-Host "Installing WSL feature." -ForegroundColor DarkCyan
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
 } 
 
-if($(Get-WindowsOptionalFeature -FeatureName Containers -Online).State -ieq 'disabled')
-{
-    Write-Host "Installing Containers feature." -ForegroundColor DarkCyan
-    Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -NoRestart
+if ($(Get-WindowsOptionalFeature -FeatureName MicrosoftWindowsPowerShellV2Root -Online).State -ieq 'disabled') {
+    Write-Host "Installing PowerShell 2.0 feature." -ForegroundColor DarkCyan
+    Enable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root -All -NoRestart
+
 }
 
 $confirmation = Read-Host "A restart is required for the changes to take effect. Restart now (y/n)?"
