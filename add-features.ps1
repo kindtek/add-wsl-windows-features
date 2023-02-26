295453002cfe4abc306af731106dd2666d9bcff9
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-26 04:13:20 -0800

    add newline

--- add-features.ps1
+++ add-features.ps1
@@ -2,7 +2,7 @@ Write-Host "`r`nThe following Windows features will now be installed:" -Foregrou
 Write-Host "`t- Hyper-V`r`n`t- Virtual Machine Platform`r`n`t- Containers`r`n`t- Windows Linux Subsystem`r`n`t- Powershell 2.0" -ForegroundColor Magenta
 
 $new_install = $false
-$install = Read-Host "`r`nPress ENTER to continue or enter `"q`" to quit"
+$install = Read-Host "`r`nPress ENTER to continue or enter `"q`" to quit`r`n"
 if ($install -ieq 'quit' -Or $install -ieq 'q') { 
     Write-Host "skipping $software_name install and exiting..."
     exit
