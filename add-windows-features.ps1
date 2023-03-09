36d4567b7b9de9249711bb5146eb1b473cc0bd62
kindtek <n8@kindtek.io>
2023-03-09 15:14:26 +0000

    add missing space

--- add-windows-features.ps1
+++ add-windows-features.ps1
@@ -1,5 +1,5 @@
 Write-Host "`r`nThe following Windows features will now be installed:" -ForegroundColor Magenta
-Write-Host "`t- Hyper-V`r`n`t- Virtual Machine Platform`r`n`t- Powershell 2.0`r`n`t- Containers`r`n`t-.NET Framework 3.5`r`n`t- Windows Linux Subsystem`r`n`t" -ForegroundColor Magenta
+Write-Host "`t- Hyper-V`r`n`t- Virtual Machine Platform`r`n`t- Powershell 2.0`r`n`t- Containers`r`n`t- .NET Framework 3.5`r`n`t- Windows Linux Subsystem`r`n`t" -ForegroundColor Magenta
 
 
 $new_install = $false
