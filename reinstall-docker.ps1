4be675f25c3eba056844fbe21e46266a084e612f
kindtek@github.com <n8@kindtek.io>
2023-06-02 04:44:16 -0700

    no confirmation

--- reinstall-docker.ps1
+++ reinstall-docker.ps1
@@ -1,8 +1,8 @@
 docker builder prune -af
 docker system prune -af --volumes
 winget uninstall --id=Docker.DockerDesktop
-Remove-Item "$env:APPDATA\Docker"
-Remove-Item "$env:LOCALAPPDATA\Docker"
-Remove-Item "$env:USERPROFILE\.docker"
+Remove-Item "$env:APPDATA\Docker" -Force -Confirm:$false 
+Remove-Item "$env:LOCALAPPDATA\Docker" -Force -Confirm:$false 
+Remove-Item "$env:USERPROFILE\.docker" -Force -Confirm:$false
 winget install --id=Docker.DockerDesktop --location="c:\docker" --locale en-US --accept-package-agreements --accept-source-agreements
 winget upgrade --id=Docker.DockerDesktop --locale en-US --accept-package-agreements --accept-source-agreements
