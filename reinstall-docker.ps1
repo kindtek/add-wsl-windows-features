b3898c9ef48fa787398de9e6884c0eebd53e0c78
kindtek@github.com <n8@kindtek.io>
2023-06-02 04:58:21 -0700

    recurse dir

--- reinstall-docker.ps1
+++ reinstall-docker.ps1
@@ -9,7 +9,7 @@ docker builder prune -af
 docker system prune -af --volumes
 winget uninstall --id=Docker.DockerDesktop
 Remove-Item "$env:APPDATA\Docker" -Force -Confirm:$false 
-Remove-Item "$env:LOCALAPPDATA\Docker" -Force -Confirm:$false 
-Remove-Item "$env:USERPROFILE\.docker" -Force -Confirm:$false
+Remove-Item "$env:LOCALAPPDATA\Docker" -Recurse -Force -Confirm:$false 
+Remove-Item "$env:USERPROFILE\.docker" -Recurse -Force -Confirm:$false
 winget install --id=Docker.DockerDesktop --location="c:\docker" --locale en-US --accept-package-agreements --accept-source-agreements
 winget upgrade --id=Docker.DockerDesktop --locale en-US --accept-package-agreements --accept-source-agreements
