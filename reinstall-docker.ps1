8e3f79485f33a246044c3a6e2e6a6cbdedf53dd7
kindtek@github.com <n8@kindtek.io>
2023-06-01 23:13:14 -0700

    remove docker local appdata

--- reinstall-docker.ps1
+++ reinstall-docker.ps1
@@ -1,3 +1,5 @@
 winget uninstall --id=Docker.DockerDesktop
+Remove-Item "$env:APPDATA\Docker"
+Remove-Item "$env:LOCALAPPDATA\Docker"
 winget install --id=Docker.DockerDesktop --location="c:\docker" --locale en-US --accept-package-agreements --accept-source-agreements
 winget upgrade --id=Docker.DockerDesktop --locale en-US --accept-package-agreements --accept-source-agreements
