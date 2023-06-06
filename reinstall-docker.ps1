d8ccd934bd1270f3d53a69c378ec8afeafb3db95
kindtek <98300737+kindtek@users.noreply.github.com>
2023-06-05 17:33:15 -0700

    remove .docker-installed

--- reinstall-docker.ps1
+++ reinstall-docker.ps1
@@ -19,3 +19,9 @@ Write-Host "Hit ENTER to proceed with Docker Desktop installation"
 # winget install --id=Docker.DockerDesktop --location="c:\docker" --locale en-US --accept-package-agreements --accept-source-agreements
 winget install --id=Docker.DockerDesktop --locale en-US --accept-package-agreements --accept-source-agreements
 winget upgrade --id=Docker.DockerDesktop --locale en-US --accept-package-agreements --accept-source-agreements
+Invoke-WebRequest -Uri https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe -OutFile DockerDesktopInstaller.exe
+.\DockerDesktopInstaller.exe
+# & 'C:\Program Files\Docker\Docker\Docker Desktop.exe'
+# "Docker Desktop Installer.exe" install --accept-license --backend=wsl-2 --installation-dir=c:\docker 
+Remove-Item "$env:USERPROFILE/repos/kindtek/dvlw/.docker-installed" -Force -ErrorAction SilentlyContinue
+Remove-Item "DockerDesktopInstaller.exe" -Force -ErrorAction SilentlyContinue
